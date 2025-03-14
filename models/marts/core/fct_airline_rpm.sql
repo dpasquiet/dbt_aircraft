ec422715d0c8aee4490d227c2f262b04f5214bc6
David PASQUIET <dpasquiet@gmail.com>
2025-03-14 08:43:03 +0000

    Commit fct_airline

--- models/marts/core/fct_airline_rpm.sql
+++ models/marts/core/fct_airline_rpm.sql
@@ -12,6 +12,18 @@ best_rpm as (
     join flight_summary using ("Airline_Code")
     GROUP BY "Airline_Name", YEAR(TO_DATE("Date"))
     ORDER BY "Airline_Name", RPM_total DESC
+),
+
+ranked_benefices AS (
+    SELECT "Airline_Name", year, RPM_total,
+        ROW_NUMBER() OVER (PARTITION BY "Airline_Name" ORDER BY RPM_total DESC) AS rang
+    FROM best_rpm
 )
 
-select * from best_rpm
+-- select * from ranked_benefices
+
+SELECT "Airline_Name", year, RPM_total
+FROM ranked_benefices
+WHERE rang = 1
+
+--select * from best_rpm
